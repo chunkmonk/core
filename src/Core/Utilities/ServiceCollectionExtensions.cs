57907767942f5fd5aa13baa65caa3a57ed820fe2
Ubuntu <ubuntu@ip-10-0-0-198.us-east-2.compute.internal>
2018-09-07 00:31:01 +0000

    Testing

--- src/Core/Utilities/ServiceCollectionExtensions.cs
+++ src/Core/Utilities/ServiceCollectionExtensions.cs
@@ -69,7 +69,7 @@ namespace Bit.Core.Utilities
         public static void AddDefaultServices(this IServiceCollection services, GlobalSettings globalSettings)
         {
             services.AddSingleton<IMailService, HandlebarsMailService>();
-            services.AddSingleton<ILicensingService, LicensingService>();
+            services.AddSingleton<ILicensingService, NoopLicensingService>();
             services.AddSingleton<IApplicationCacheService, InMemoryApplicationCacheService>();
 
             if(CoreHelpers.SettingHasValue(globalSettings.Mail.SendGridApiKey))
